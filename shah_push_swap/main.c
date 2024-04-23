a006d83821c4eff49c93a80cbbdaa6af7375ea0a
Asmaalawlaqi <143976152+Asmaalawlaqi@users.noreply.github.com>
2024-04-23 22:24:56 +0400

    main.c

--- shah_push_swap/main.c
+++ shah_push_swap/main.c
@@ -24,12 +24,12 @@ int	main(int argc, char **argv)
 	read_numbers(&stack);
 	check_for_dup_or_sorted(&stack, 0); // check any sort or dup numbers
 	create_index(&stack);
-	if (stack.stack_a_size == 2 && stack.stack_a[0] > stack.stack_a[1])
-		swap_elements("sa", stack.stack_a, stack.stack_a_size); 
-	else if (stack.stack_a_size == 3)
-		sort_three_items(&stack);
-	else if (stack.stack_a_size >= 4 && stack.stack_a_size <= 5)
-		sort_four_to_five_items(&stack);
+	if (stack.stack_a_size == 2 && stack.stack_a[0] > stack.stack_a[1]) //If the size of stack A is 2 and the number at index 0 (the first number) is greater than the number at index 1 (the second number), 
+		swap_elements("sa", stack.stack_a, stack.stack_a_size);  //hen swap them.
+	else if (stack.stack_a_size == 3) // if there is 3 numbers 
+		sort_three_items(&stack); //sort them
+	else if (stack.stack_a_size >= 4 && stack.stack_a_size <= 5) // 4 or 5 numbers  
+		sort_four_to_five_items(&stack);// sort them 
 	else
 		apply_radix_sort(&stack);
 	check_for_dup_or_sorted(&stack, 1);  // we cant have 2 dup or sorted stack
